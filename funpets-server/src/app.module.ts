7c46d67c66616ba5b193eb931a20fe54db14417f
sinedied <noda@free.fr>
2020-03-13 13:42:01 +0100

    feat: step 3

--- funpets-server/src/app.module.ts
+++ funpets-server/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoriesController } from './stories/stories.controller';
+import { Story } from './stories/story.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }),
+    TypeOrmModule.forFeature([Story])
+  ],
   controllers: [AppController, StoriesController],
   providers: [AppService],
 })
